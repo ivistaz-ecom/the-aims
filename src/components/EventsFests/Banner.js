fca0e99cf5d082e211067f94562ff63922202117
Bhavankumarg <gbhavanbkr@gmail.com>
2025-10-20 18:01:47 +0530

    home page, aims journal of research page updation

--- src/components/EventsFests/Banner.js
+++ src/components/EventsFests/Banner.js
@@ -38,6 +38,7 @@ const HeroBannerSoH = ({ announcements, pageType = "engineering" }) => {
           </div>
         </div>
 
+ 
         {/* Bottom Content Container */}
         <div className="absolute bottom-4 lg:bottom-8 left-0 right-0 px-4 lg:px-8">
           <div className="container mx-auto py-4 ">
