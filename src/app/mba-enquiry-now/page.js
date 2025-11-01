bd1cf4f825149015d03b5b7ed4dab5111a97f503
Pavan Kumar <20481A0411@gecgudlavallerumic.in>
2025-11-01 13:20:59 +0530

    Mba Landing Page Enquiry Now Page

---
+++ src/app/mba-enquiry-now/page.js
@@ -0,0 +1,7 @@
+import React from "react"
+import EnquiryNow from "@/components/MBAEnquiryNow"
+const page = () => {
+  return <EnquiryNow />
+}
+
+export default page
---
+++ src/components/MBAEnquiryNow/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import EnquiryForm from './EnquiryForm'
+
+const index = () => {
+  return (
+    <div>
+      <EnquiryForm />
+    </div>
+  )
+}
+
+export default index
